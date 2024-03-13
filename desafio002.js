21edcf6527b880f740da27c2921acdbdf8112155
Sher Dias <98842057+Shersirotzky@users.noreply.github.com>
2024-03-13 18:40:22 -0300

    Desafio Proposto pelo Felipão
    Signed-off-by: Sher Dias <98842057+Shersirotzky@users.noreply.github.com>

---
+++ desafio002.js
@@ -0,0 +1,34 @@
+let nome = 'Goku';
+let nivel = 3800;
+
+if(nivel < 1000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Ferro');
+
+}else if (nivel >= 1001 && nivel <= 2000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Cobre');
+
+}else if(nivel >= 2001 && nivel <= 3000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Bronze');
+
+}else if(nivel >= 3001 && nivel <= 4000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Prata');
+
+}else if(nivel >= 4001 && nivel <= 5000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Ouro');
+
+}else if(nivel >= 5001 && nivel <= 6000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Platina');
+
+}else if(nivel >= 6001 && nivel <= 7000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Platina Diamante');
+
+}else if(nivel >= 7001 && nivel <= 8000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Ascendente');
+
+}else if(nivel >= 8001 && nivel <= 9000){
+    console.log('O heroi de nome ' + nome, ' Está no nivel Imortal');
+
+}else {
+    console.log('O heroi de nome ' + nome, ' Está no nivel Radiante')
+}
+
